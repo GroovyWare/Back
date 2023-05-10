721499d37a3f10f6c3343733cfd16217b3e548c3
LITSII <jj417hom2000@naver.com>
2023-05-10 14:47:49 +0900

    [인태] dto,엔터티,추가

---
+++ groovy/src/main/java/com/groovy/ware/calendar/dto/CalendarDTO.java
@@ -0,0 +1,22 @@
+package com.groovy.ware.calendar.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class CalendarDTO {
+    
+    private int schCode;
+    private String schTitle;
+    private String schContext;
+    private String schDiv;
+    private DeptDto dept;
+    private EmployeeDto employee;
+    private EmployeeDto schwriter;
+    private Date schStart;
+    private Date schend;
+
+
+
+}
