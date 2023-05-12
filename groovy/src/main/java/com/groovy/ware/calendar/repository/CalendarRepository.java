6bb0593a7ee6efb26be082c6d1288cf9e300a0d7
LITSII <jj417hom2000@naver.com>
2023-05-12 09:20:19 +0900

    [이인태] 캘린더 백 시작

--- groovy/src/main/java/com/groovy/ware/calendar/repository/CalendarRepository.java
+++ groovy/src/main/java/com/groovy/ware/calendar/repository/CalendarRepository.java
@@ -10,6 +10,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.groovy.ware.calendar.entity.Calendar;
 
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+   
+   List<Calendar> findBySchTitle(String schTitle);
+
+}
+package com.groovy.ware.calendar.repository;
+
+
+
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.groovy.ware.calendar.entity.Calendar;
+
+public interface CalendarRepository extends JpaRepository<Calendar, Long> {
     
     List<Calendar> findBySchTitle(String schTitle);
 
--- groovy/src/main/java/com/groovy/ware/calendar/service/CalendarService.java
+++ groovy/src/main/java/com/groovy/ware/calendar/service/CalendarService.java
@@ -12,28 +12,29 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class CalendarService {
-    
-    private final CalendarRepository calendarRepository;
-    private final ModelMapper modelMapper;
-    private final MemberRepository memberRepository;
-    private final DepartmentRepository deptrepository;
+   
+   private final CalendarRepository calendarRepository;
+   private final ModelMapper modelMapper;
+   private final MemberRepository memberRepository;
+//    private final DepartmentRepository deptrepository;
 
-    public CalendarService(CalendarRepository calendarRepository, MemberRepository memberRepository, DepartmentRepository deptrepository, ModelMapper modelMapper)
-    {
-        this.calendarRepository = calendarRepository;
-        this.memberRepository = memberRepository;
-        this.deptrepository = deptrepository;
-        this.modelMapper = modelMapper;
-    }
-
-
-    
-    /* 1. 캘린더 메인에서 전체일정 보여주기  직원값을 가져와야한다.*/
-    public CalendarDTO viewAllSchdule() {
-        
+   public CalendarService(CalendarRepository calendarRepository, MemberRepository memberRepository, ModelMapper modelMapper)
+   {
+       this.calendarRepository = calendarRepository;
+       this.memberRepository = memberRepository;
+//        this.deptrepository = deptrepository;
+       this.modelMapper = modelMapper;
+   }
 
 
-    }
+   
+   /* 1. 캘린더 메인에서 전체일정 보여주기  직원값을 가져와야한다.*/
+//    public CalendarDTO viewAllSchdule() {
+//        
+//        List<
+//
+//
+//    }
 
 
 
