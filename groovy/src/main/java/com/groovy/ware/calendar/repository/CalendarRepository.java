673cf22fa5025976882c1e869f4d9d8907cfc7aa
LITSII <jj417hom2000@naver.com>
2023-05-12 09:23:04 +0900

    commit

--- groovy/src/main/java/com/groovy/ware/calendar/repository/CalendarRepository.java
+++ groovy/src/main/java/com/groovy/ware/calendar/repository/CalendarRepository.java
@@ -14,19 +14,3 @@ public interface CalendarRepository extends JpaRepository<Calendar, Long> {
    List<Calendar> findBySchTitle(String schTitle);
 
 }
-package com.groovy.ware.calendar.repository;
-
-
-
-import java.util.List;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.groovy.ware.calendar.entity.Calendar;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {
-    
-    List<Calendar> findBySchTitle(String schTitle);
-
-}
