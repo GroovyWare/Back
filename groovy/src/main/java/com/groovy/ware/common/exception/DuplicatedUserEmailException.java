7c740b6b82fc8d85c105e062dfd013f105cb4d43
P.MINKYOUNG <tofhddl94@gmail.com>
2023-05-12 12:28:32 +0900

    [박민경] 결재 문서 요청 진행중

---
+++ groovy/src/main/java/com/groovy/ware/common/exception/DuplicatedUserEmailException.java
@@ -0,0 +1,9 @@
+package com.groovy.ware.common.exception;
+
+public class DuplicatedUserEmailException extends RuntimeException {
+	
+	public DuplicatedUserEmailException(String msg) {
+		super(msg);
+	}
+
+}
