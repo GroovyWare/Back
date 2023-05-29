b66e749c2bf431e5f9bae7dddb9d75ae5b30be1c
LITS <jj417hom2000@gmail.com>
2023-05-30 02:40:17 +0900

    [이인태] 컨트롤러 일부 수정

--- groovy/src/main/java/com/groovy/ware/attendance/controller/AttendanceController.java
+++ groovy/src/main/java/com/groovy/ware/attendance/controller/AttendanceController.java
@@ -73,12 +73,12 @@ public class AttendanceController {
     
         attendanceDto.setAttDate(new Date(System.currentTimeMillis()));
     
-        attendanceDto.setEmployee(employee);
+        attendanceDto.setEmployee(employee); 
         
         /* 조회 메소드를 이용해서 출근 시간에 대한 변경을 없앤다. */
         AttendanceDto findStartTime = attendanceService.viewMain(employee);
         attendanceDto.setAttStart(findStartTime.getAttStart());
-
+       
     
         attendanceService.leaveWork(attendanceDto, employee); 
     
